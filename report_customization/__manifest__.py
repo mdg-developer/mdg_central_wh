76efc4b09987f36c86c217d4d56aa1c833ef7828
Hnin <hnin.7thcomputing@gmail.com>
2021-06-14 14:11:00 +0630

    Picking Operation Print

---
+++ report_customization/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Report Customization',
+    "version": "14.0.0.0.0",
+    'author': '7thcomputing',
+    'website': 'https://www.7thcomputing.com',
+    'license': 'LGPL-3',
+    'installable': True,
+    'summary': 'Report Customization',
+    'depends': [
+        'base',
+        'stock',
+    ],
+    'data': [
+             'reports/report_stockpicking_operations.xml',
+    ],
+}
