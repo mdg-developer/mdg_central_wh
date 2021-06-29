b1d32b0cf700c4da7763212a6ded717976e4c887
Hnin <hnin.7thcomputing@gmail.com>
2021-06-29 14:38:42 +0630

    MDGWH-171::Seperate stock move line base on pallet QTy when validate PO reciept record

--- inventory_customization/models/stock_picking.py
+++ inventory_customization/models/stock_picking.py
@@ -13,6 +13,8 @@ class StockPicking(models.Model):
     def split_operation_lines(self):
         for line in self.move_ids_without_package:
             if line.product_id.product_tmpl_id.pallet_quantity > 0:
+                putaway_location = None
+                existing_locations=[]
                 divisor =  math.ceil(line.product_uom_qty / line.product_id.product_tmpl_id.pallet_quantity)
                 allocated_qty = 0
                 for counter in range(0, int(divisor)):     
@@ -22,23 +24,41 @@ class StockPicking(models.Model):
                         quantity = line.product_uom_qty - allocated_qty
                     if counter == 0:
                         existing_move_line = self.env['stock.move.line'].search([('picking_id', '=', self.id),('product_id', '=', line.product_id.id)])
-                        if existing_move_line:
+                        if existing_move_line:                            
                             lot_name = self.env['ir.sequence'].next_by_code('stock.lot.serial')
                             existing_move_line.write({
                                                         'qty_done': quantity,
                                                         'lot_name': lot_name,
-                                                    })
+                                                    })                            
                             allocated_qty = allocated_qty + quantity
+                            location_dest_id = existing_move_line.location_dest_id.id
+                            existing_locations.append(location_dest_id)
                     else:
-                        lot_name = self.env['ir.sequence'].next_by_code('stock.lot.serial')
+                        putaway_rules = line.location_dest_id.putaway_rule_ids.filtered(lambda x: x.product_id == line.product_id)
+                        if putaway_rules:
+                            putaway_location = putaway_rules[0].location_out_id
+                            for rule in putaway_rules:
+                                records = self.env['stock.quant'].search([('location_id','=',rule.location_out_id.id),('product_id','=',line.product_id.id)])
+                                existed  = rule.location_out_id.exist_location(rule.location_out_id,existing_locations)
+                                if  existed == False and len(records) == 0:                        
+                                    putaway_location = rule.location_out_id
+                                    existing_locations.append(putaway_location)
+                                    break
+                                else:
+                                    continue
+                        lot_value = {                                        
+                                        'product_id': line.product_id.id,
+                                        'company_id': line.company_id.id,
+                                    }
+                        product_lot = self.env['stock.production.lot'].create(lot_value)
                         values = {
                                     'picking_id': self.id,
                                     'product_id': line.product_id.id,
                                     'qty_done': quantity,
                                     'product_uom_id': line.product_uom.id,
                                     'location_id': line.location_id.id,
-                                    'location_dest_id': line.location_dest_id.id,
-                                    'lot_name': lot_name,
+                                    'location_dest_id': putaway_location.id if putaway_location else None,
+                                    'lot_id': product_lot.id,                                    
                                 }
                         self.env['stock.move.line'].create(values)
                         allocated_qty = allocated_qty + quantity
@@ -46,9 +66,10 @@ class StockPicking(models.Model):
     def button_validate(self):
         
         result = super(StockPicking, self).button_validate()
-        picking = self.env['stock.picking'].search([('origin', '=', self.origin),('name', 'ilike', 'INT')])
-        if picking:
-            picking.split_operation_lines()
+        if self.picking_type_id.sequence_code == 'IN': 
+            picking = self.env['stock.picking'].search([('origin', '=', self.origin),('name', 'ilike', 'INT')])
+            if picking:
+                picking.split_operation_lines()
         return result
         
                     
