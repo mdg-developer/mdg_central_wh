7fe134a61753ccd52f8938041047bc59a3770ce5
MDG DEV <mdgdevuser@gmail.com>
2022-02-04 14:45:46 +0630

    Add Field in Barcode Screen

---
+++ stock_barcode_customization/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Barcode Customization',
+    "version": "15.0.0.0.0",
+    'author': '',
+    'website': '',
+    'license': 'LGPL-3',
+    'installable': True,
+    'summary': 'Barcode Customization',
+    'depends': [
+        'base',
+        'stock',
+        'stock_barcode'
+    ],
+    'data': [
+             'views/stock_barcode_view.xml',
+    ],
+}
---
+++ stock_barcode_customization/models/stock_move_line.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from collections import Counter
+
+from odoo import _, api, fields, tools, models
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools import OrderedSet
+from odoo.tools.float_utils import float_compare, float_is_zero, float_round
+from odoo.addons.base.models.ir_model import MODULE_UNINSTALL_FLAG
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    bigger_uom_qty_done = fields.Float('Bigger UOM Done', default=0.0, digits='Product Unit of Measure', copy=False)
+    basic_uom_qty_done = fields.Float('Basic UOM Done', default=0.0, digits='Product Unit of Measure', copy=False)
+    product_purchase_uom_id = fields.Many2one(related='product_id.uom_po_id')
+
+    @api.onchange('product_id', 'bigger_uom_qty_done','basic_uom_qty_done','product_purchase_uom_id')
+    def _onchange_product_id_uom(self):
+        if self.product_id.uom_id:
+            self.product_uom_id = self.product_id.uom_id.id
+        if self.product_id and self.product_purchase_uom_id:
+            other_uom_qty = 0.0
+            if self.product_purchase_uom_id.uom_type == 'bigger':
+                other_uom_qty = self.bigger_uom_qty_done * self.product_purchase_uom_id.factor_inv
+            self.qty_done = other_uom_qty + self.basic_uom_qty_done
