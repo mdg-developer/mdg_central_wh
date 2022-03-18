d030c81b7f3c7ab9ec59670da7be607c2af7004d
MDG DEV <mdgdevuser@gmail.com>
2022-03-18 16:25:52 +0630

    MDGWH-211:update the expiration date when overwrite or update the expiry date in INT

--- stock_barcode_customization/models/stock_expiration_date.py
+++ stock_barcode_customization/models/stock_expiration_date.py
@@ -7,8 +7,17 @@ from odoo.exceptions import UserError
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    expiration_date = fields.Date(string='Expiry Date', help='This is the date on which the goods with this Serial Number may'
+    expiration_date = fields.Date(related='lot_id.expiration_date',readonly=False,string='Expiry Date', help='This is the date on which the goods with this Serial Number may'
                                                                  ' become dangerous and must not be consumed.')
+
+    def write(self, vals):
+
+        res = super(StockMoveLine, self).write(vals)
+        if 'expiration_date' in vals and self.lot_id:
+            self.lot_id.update({
+                'expiration_date':self.expiration_date
+            })
+        return res
 
 
 class StockPicking(models.Model):
@@ -21,14 +30,8 @@ class StockPicking(models.Model):
                     raise UserError(_("Expiry Date is Required!"))
                     return
         return super(StockPicking, self).button_validate()
-        # if self.picking_type_id.sequence_code == 'IN':
-        #     picking = self.env['stock.picking'].search([('origin', '=', self.origin), ('name', 'ilike', 'INT')])
-        #     if picking:
-        #         for flag in picking.move_line_ids_without_package:
-        #             existing_picking_line = self.move_line_ids_without_package.filtered(lambda r : r.product_id.id ==flag.product_id.id);
-        #             if existing_picking_line and existing_picking_line.expiration_date:
-        #                 flag.expiration_date = existing_picking_line.expiration_date
-        # return result
+
+
 
 
 
