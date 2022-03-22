f20beb1d6e308acac728d541125d9f5f8353f866
MDG DEV <mdgdevuser@gmail.com>
2022-03-22 14:33:15 +0630

    MDGWH:208 : Check scan product issue, bug fix

--- stock_barcode_customization/static/src/components/stock_barcode_main.js
+++ stock_barcode_customization/static/src/components/stock_barcode_main.js
@@ -7,7 +7,7 @@ const _t = core._t;
 patch(MainComponent.prototype, 'stock_barcode_customization', {
 
       get sourceLocationsLength() {
-            console.log("Inside sourceLocationsLength",this.env.model.locationList.length)
+
             if(this.env.model.locationList.length>5){
                 return true
             }
--- stock_barcode_customization/static/src/models/stock_barcode_picking_model.js
+++ stock_barcode_customization/static/src/models/stock_barcode_picking_model.js
@@ -6,17 +6,21 @@ const _t = core._t;
 
 patch(BarcodePickingModel.prototype, 'stock_barcode_customization', {
     async changeDestinationLocation(id, moveScannedLineOnly) {
-        console.log("In Barcode Picking Model : in line 36")
-        console.log("id",id)
-        console.log("moveScannedLineOnly",moveScannedLineOnly)
-        console.log("previousScannedLines",this.previousScannedLines)
         this.currentDestLocationId = id;
         if (moveScannedLineOnly && this.previousScannedLines.length) {
             this.currentDestLocationId = id;
-            for (const line of this.previousScannedLines) {
-                line.location_dest_id = id;
-                this._markLineAsDirty(line);
+            if (this.previousScannedLines.length != 1){
+                this.lastScannedLine.location_dest_id = id;
+                this._markLineAsDirty(this.lastScannedLine);
             }
+            else{
+                for (const line of this.previousScannedLines) {
+                    line.location_dest_id = id;
+                    this._markLineAsDirty(line);
+                }
+
+            }
+
         } else {
             // If the button was used to change the location, if will change the
             // destination location of all the page's move lines.
