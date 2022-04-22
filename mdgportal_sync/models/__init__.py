532c4551c3323502688acb429b6553d8804f7beb
MDG DEV <mdgdevuser@gmail.com>
2022-04-22 16:05:59 +0630

    MDGWH-195:sync so and bgin auto issue quantity

--- mdgportal_sync/models/__init__.py
+++ mdgportal_sync/models/__init__.py
@@ -1,2 +1,3 @@
 from . import portal_connection
 from . import stock_return
+from . import bgin_so
---
+++ mdgportal_sync/models/bgin_so.py
@@ -0,0 +1,30 @@
+from xmlrpc import client
+from odoo import models, fields, api, _
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    def button_validate(self):
+        result = super(StockPicking, self).button_validate()
+        if self.picking_type_id.code == "outgoing" and self.state == 'done':
+            sale_order_id = self.env['sale.order'].search([('name', '=', self.origin)], limit=1)
+            if sale_order_id.origin.startswith("BRFI"):
+                sd_uid, url, db, password = self.env['cwh.connection'].get_connection_data()
+                models = client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+                brfi_name = sale_order_id.origin
+
+
+                bgin_id = models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'search',
+                                           [[['request_id.name', '=', brfi_name]]], {'limit': 1})
+                if bgin_id:
+                    models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'approve', [bgin_id])
+                    for line in self.move_ids_without_package:
+                        if line.quantity_done != 0:
+                            change_bgin_line_id = models.execute_kw(db, sd_uid, password, 'branch.good.issue.note.line','search', [
+                                                                        [['line_id', '=', bgin_id[0]], ['product_id.default_code', '=', line.product_id.default_code]]], {'limit': 1})
+                            value = {
+                                'issue_quantity': line.quantity_done
+                            }
+                            models.execute_kw(db, sd_uid, password, 'branch.good.issue.note.line', 'write', [change_bgin_line_id, value])
+                    models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'issue', [bgin_id])
+        return result
