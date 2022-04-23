0945dc2bc2de21e5cae0210ba260fdfca6a9a5ed
MDG DEV <mdgdevuser@gmail.com>
2022-04-23 14:00:14 +0630

    MDGWH-195: fix wrong issue quantity in syncing

--- mdgportal_sync/models/bgin_so.py
+++ mdgportal_sync/models/bgin_so.py
@@ -8,21 +8,22 @@ class StockPicking(models.Model):
         result = super(StockPicking, self).button_validate()
         if self.picking_type_id.code == "outgoing" and self.state == 'done':
             sale_order_id = self.env['sale.order'].search([('name', '=', self.origin)], limit=1)
-            if sale_order_id.origin.startswith("BRFI"):
-                sd_uid, url, db, password = self.env['cwh.connection'].get_connection_data()
-                models = client.ServerProxy('{}/xmlrpc/2/object'.format(url))
-                brfi_name = sale_order_id.origin
+            if sale_order_id.origin:
+                if sale_order_id.origin.startswith("BRFI"):
+                    sd_uid, url, db, password = self.env['cwh.connection'].get_connection_data()
+                    models = client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+                    brfi_name = sale_order_id.origin
 
 
-                bgin_id = models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'search',
-                                           [[['request_id.name', '=', brfi_name]]], {'limit': 1})
-                if bgin_id:
-                    models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'approve', [bgin_id])
-                    for line in sale_order_id.order_line:
-                        change_bgin_line_id = models.execute_kw(db, sd_uid, password, 'branch.good.issue.note.line','search', [[['line_id', '=', bgin_id[0]],['product_id.default_code', '=',line.product_id.default_code],['product_uom.name','=',line.product_uom.name]]],{'limit' :1})
-                        value = {
-                            'issue_quantity': line.qty_delivered
-                        }
-                        models.execute_kw(db, sd_uid, password, 'branch.good.issue.note.line', 'write',[change_bgin_line_id, value])
-                    models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'issue', [bgin_id])
+                    bgin_id = models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'search',
+                                               [[['request_id.name', '=', brfi_name]]], {'limit': 1})
+                    if bgin_id:
+                        models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'approve', [bgin_id])
+                        for line in sale_order_id.order_line:
+                            change_bgin_line_id = models.execute_kw(db, sd_uid, password, 'branch.good.issue.note.line','search', [[['line_id', '=', bgin_id[0]],['product_id.default_code', '=',line.product_id.default_code],['product_uom.name','=',line.product_uom.name]]],{'limit' :1})
+                            value = {
+                                'issue_quantity': line.qty_delivered
+                            }
+                            models.execute_kw(db, sd_uid, password, 'branch.good.issue.note.line', 'write',[change_bgin_line_id, value])
+                        models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'issue', [bgin_id])
         return result
--- mdgportal_sync/models/stock_return.py
+++ mdgportal_sync/models/stock_return.py
@@ -8,20 +8,21 @@ class StockPicking(models.Model):
         result = super(StockPicking, self).button_validate()
         # Check if the stock picking transaction is coming from mdgportal ; assume that coming transaction start with
         # 'SRN' in Source Document
-        if self.origin.startswith("SRN") and self.state == 'done':
-            sd_uid, url, db, password = self.env['cwh.connection'].get_connection_data()
-            common = client.ServerProxy('{}/xmlrpc/2/common'.format(url))
-            models = client.ServerProxy('{}/xmlrpc/2/object'.format(url))
-            srn_id = models.execute_kw(db, sd_uid, password, 'stock.return', 'search', [[['name', '=', self.origin]]],{'limit': 1})
-            if srn_id:
-                for line in self.move_ids_without_package:
-                    if line.quantity_done != 0:
-                        change_sr_line_id = models.execute_kw(db, sd_uid, password, 'stock.return.line', 'search',[[['line_id', '=', srn_id[0]],['product_id.default_code','=',line.product_id.default_code]]],{'limit':1})
-                        value ={
-                            'actual_return_quantity':line.quantity_done
-                        }
-                        models.execute_kw(db, sd_uid, password, 'stock.return.line', 'write',[change_sr_line_id, value])
+        if self.origin:
+            if self.origin.startswith("SRN") and self.state == 'done':
+                sd_uid, url, db, password = self.env['cwh.connection'].get_connection_data()
+                common = client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+                models = client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+                srn_id = models.execute_kw(db, sd_uid, password, 'stock.return', 'search', [[['name', '=', self.origin]]],{'limit': 1})
+                if srn_id:
+                    for line in self.move_ids_without_package:
+                        if line.quantity_done != 0:
+                            change_sr_line_id = models.execute_kw(db, sd_uid, password, 'stock.return.line', 'search',[[['line_id', '=', srn_id[0]],['product_id.default_code','=',line.product_id.default_code]]],{'limit':1})
+                            value ={
+                                'actual_return_quantity':line.quantity_done
+                            }
+                            models.execute_kw(db, sd_uid, password, 'stock.return.line', 'write',[change_sr_line_id, value])
 
-                models.execute_kw(db, sd_uid, password, 'stock.return', 'received',srn_id)
+                    models.execute_kw(db, sd_uid, password, 'stock.return', 'received',srn_id)
 
         return result
