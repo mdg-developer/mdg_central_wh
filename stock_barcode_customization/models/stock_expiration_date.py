b179d07f5a1f081b613963ed2bb7a7d2b825d7c7
MDG DEV <mdgdevuser@gmail.com>
2022-05-10 16:33:41 +0630

    MDGWH-217:Expiry date doesn't store in receipt process

--- stock_barcode_customization/models/stock_expiration_date.py
+++ stock_barcode_customization/models/stock_expiration_date.py
@@ -7,17 +7,28 @@ from odoo.exceptions import UserError
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    expiration_date = fields.Date(related='lot_id.expiration_date',readonly=False,string='Expiry Date', help='This is the date on which the goods with this Serial Number may'
+    expiration_date = fields.Date(related='lot_id.expiration_date',store=True,readonly=False,string='Expiry Date', help='This is the date on which the goods with this Serial Number may'
                                                                  ' become dangerous and must not be consumed.')
 
     def write(self, vals):
-
         res = super(StockMoveLine, self).write(vals)
         if 'expiration_date' in vals and self.lot_id:
             self.lot_id.update({
                 'expiration_date':self.expiration_date
             })
         return res
+
+    def _create_and_assign_production_lot(self):
+        """ Creates and assign new production lots for move lines."""
+        lot_vals = [{
+            'company_id': ml.move_id.company_id.id,
+            'name': ml.lot_name,
+            'product_id': ml.product_id.id,
+            'expiration_date':ml.expiration_date,
+        } for ml in self]
+        lots = self.env['stock.production.lot'].create(lot_vals)
+        for ml, lot in zip(self, lots):
+            ml._assign_production_lot(lot)
 
 
 class StockPicking(models.Model):
