bc64a8272cde9477bff334b6604d47087169a5f4
MDG DEV <mdgdevuser@gmail.com>
2022-05-23 16:17:10 +0630

    fix bug : constraint on package

--- stock_barcode_customization/models/stock_move_line.py
+++ stock_barcode_customization/models/stock_move_line.py
@@ -12,12 +12,12 @@ from odoo.addons.base.models.ir_model import MODULE_UNINSTALL_FLAG
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    _sql_constraints = [
-        (
-            'unique_result_package_id', 'UNIQUE(result_package_id,picking_code)',
-            'Destinatin Package Must be unique!')
-    ]
-    picking_code = fields.Selection(related='picking_id.picking_type_id.code', readonly=True, store=True)
+    # _sql_constraints = [
+    #     (
+    #         'unique_result_package_id', 'UNIQUE(result_package_id,picking_code)',
+    #         'Destinatin Package Must be unique!')
+    # ]
+    # picking_code = fields.Selection(related='picking_id.picking_type_id.code', readonly=True, store=True)
     bigger_uom_qty_done = fields.Float('Bigger UOM Done', default=0.0, digits='Product Unit of Measure', copy=False)
     basic_uom_qty_done = fields.Float('Basic UOM Done', default=0.0, digits='Product Unit of Measure', copy=False)
     # product_purchase_uom_id = fields.Many2one(related='product_id.uom_po_id')
