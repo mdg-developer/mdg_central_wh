b40d759f1fbd0c6acd9d774054f903ace31e533f
MDG DEV <mdgdevuser@gmail.com>
2022-06-23 09:32:56 +0630

    fix issue of duplicate so coming from bgin

--- mdgportal_sync/models/bgin_so.py
+++ mdgportal_sync/models/bgin_so.py
@@ -19,7 +19,7 @@ class StockPicking(models.Model):
                     bgin_id = models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'search',
                                                [[['request_id.name', '=', brfi_name]]], {'limit': 1})
                     if bgin_id:
-                        models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'approve', [bgin_id])
+                        # models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'approve', [bgin_id])
                         for line in sale_order_id.order_line:
                             change_bgin_line_id = models.execute_kw(db, sd_uid, password, 'branch.good.issue.note.line','search', [[['line_id', '=', bgin_id[0]],['product_id.default_code', '=',line.product_id.default_code],['product_uom.name','=',line.product_uom.name]]],{'limit' :1})
                             value = {
