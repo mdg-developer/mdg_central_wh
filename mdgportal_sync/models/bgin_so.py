34a7096e22388d5ad5db145a3d0db2160e222b33
MDG DEV <mdgdevuser@gmail.com>
2022-11-30 10:19:15 +0630

    partial issue ,duplicate sale order,bgin state

--- mdgportal_sync/models/bgin_so.py
+++ mdgportal_sync/models/bgin_so.py
@@ -8,6 +8,7 @@ class StockPicking(models.Model):
         result = super(StockPicking, self).button_validate()
         backorder = self.env['stock.picking'].search([('sale_id', '=', self.sale_id.id),('backorder_id', '=', self.id )])
         if self.picking_type_id.code == "outgoing" and self.state == 'done':
+
             if not self.backorder_id and not backorder and not self.gin_ref:
                 sale_order_id = self.env['sale.order'].search([('name', '=', self.origin)], limit=1)
                 if sale_order_id.origin:
@@ -92,26 +93,20 @@ class StockPicking(models.Model):
                         bgin_id = models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'search',
                                                     [[['request_id.name', '=', brfi_name],['state', '=', 'approve']]], {'limit': 1})
                         if bgin_id:
-                            # models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'set_issue_qty_zero',
-                            #                   [bgin_id])
-                            for line in sale_order_id.order_line:
-                                flag_qty_delivered = 0
-                                # BGIN Issue Syncing
-                                same_prod = self.env['sale.order.line'].search([('product_id', '=', line.product_id.id), ('order_id', '=', sale_order_id.id),
-                                     ('loose', '=', line.loose)])
-                                for flag in same_prod:
-                                    flag_qty_delivered += flag.qty_delivered
+                            models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'set_issue_qty_zero',
+                                              [bgin_id])
+                            for line in self.move_ids_without_package:
+                                issue_qty =0
+                                factor = line.product_id.uom_po_id.factor_inv
+                                if factor != 0:
+                                    issue_qty = line.quantity_done/factor
 
                                 # BGIN Issue Syncing
-                                change_bgin_line_id = models.execute_kw(db, sd_uid, password, 'branch.good.issue.note.line','search', [[['line_id', '=', bgin_id[0]],['product_id.default_code', '=',line.product_id.default_code],['product_uom.name','=',line.product_uom.name]]],{'limit' :1})
-                                if flag_qty_delivered != 0:
-                                    value = {
-                                        'issue_quantity': flag_qty_delivered
-                                    }
-                                else:
-                                    value = {
-                                        'issue_quantity': line.qty_delivered
-                                    }
+                                change_bgin_line_id = models.execute_kw(db, sd_uid, password, 'branch.good.issue.note.line','search', [[['line_id', '=', bgin_id[0]],['product_id.default_code', '=',line.product_id.default_code]]],{'limit' :1})
+                                value = {
+                                    'issue_quantity': issue_qty
+                                }
+
                                 models.execute_kw(db, sd_uid, password, 'branch.good.issue.note.line', 'write',[change_bgin_line_id, value])
                             models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'issue', [bgin_id])
                             gin_id = models.execute_kw(db, sd_uid, password, 'branch.stock.requisition', 'create_gin', [brfi_id])
@@ -130,26 +125,20 @@ class StockPicking(models.Model):
                         bgin_id = models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'search',
                                                     [[['request_id.name', '=', brfi_name],['state', '=', 'approve']]], {'limit': 1})
                         if bgin_id:
-                            # models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'set_issue_qty_zero',
-                            #                   [bgin_id])
-                            for line in sale_order_id.order_line:
-                                flag_qty_delivered = 0
-                                # BGIN Issue Syncing
-                                same_prod = self.env['sale.order.line'].search([('product_id', '=', line.product_id.id), ('order_id', '=', sale_order_id.id),
-                                     ('loose', '=', line.loose)])
-                                for flag in same_prod:
-                                    flag_qty_delivered += flag.qty_delivered
+                            models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'set_issue_qty_zero',
+                                              [bgin_id])
+                            for line in self.move_ids_without_package:
+                                issue_qty =0
+                                factor = line.product_id.uom_po_id.factor_inv
+                                if factor != 0:
+                                    issue_qty = line.quantity_done/factor
 
                                 # BGIN Issue Syncing
-                                change_bgin_line_id = models.execute_kw(db, sd_uid, password, 'branch.good.issue.note.line','search', [[['line_id', '=', bgin_id[0]],['product_id.default_code', '=',line.product_id.default_code],['product_uom.name','=',line.product_uom.name]]],{'limit' :1})
-                                if flag_qty_delivered != 0:
-                                    value = {
-                                        'issue_quantity': flag_qty_delivered
-                                    }
-                                else:
-                                    value = {
-                                        'issue_quantity': line.qty_delivered
-                                    }
+                                change_bgin_line_id = models.execute_kw(db, sd_uid, password, 'branch.good.issue.note.line','search', [[['line_id', '=', bgin_id[0]],['product_id.default_code', '=',line.product_id.default_code]]],{'limit' :1})
+                                value = {
+                                    'issue_quantity': issue_qty
+                                }
+
                                 models.execute_kw(db, sd_uid, password, 'branch.good.issue.note.line', 'write',[change_bgin_line_id, value])
                             models.execute_kw(db, sd_uid, password, 'branch.good.issue.note', 'issue', [bgin_id])
 
