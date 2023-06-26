c130da4240a0af8cbcdd3da5e2434c902faece30
SHL MDG <developer@mdgmyanmar.com>
2023-06-26 08:47:19 +0630

    To make inventory adjustment file importing fine.

--- inventory_customization/models/stock_quant_package.py
+++ inventory_customization/models/stock_quant_package.py
@@ -10,3 +10,14 @@ class QuantPackage(models.Model):
     name = fields.Char(
         'Package Reference', copy=False, index=True,
         default=lambda self: self.env['ir.sequence'].next_by_code('stock.quant.package') or '')
+
+class StockQuant(models.Model):
+    _inherit = 'stock.quant'
+
+    @api.model
+    def _get_inventory_fields_create(self):
+        """ Returns a list of fields user can edit when he want to create a quant in `inventory_mode`.
+        """
+        res = super(StockQuant,self)._get_inventory_fields_create()
+        res += ['inventory_quantity']
+        return res
